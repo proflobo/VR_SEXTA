8f149064e68d5ca196fefefdcacf7250f40a468b
lobo <matheus.v.lobo@gmail.com>
2020-05-13 23:57:15 -0300

    minha cena

--- Assets/Scripts/gira_OBJ.cs
+++ Assets/Scripts/gira_OBJ.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class gira_OBJ : MonoBehaviour
 {
-    public float OBJspeed;
-    public float OBJspeedRot;
+    public float SaiAndando;
+    public float RotY, RotX, RotZ;
+
+   // public float smooth;
+   // public float tiltAngle;
 
     void Start()
     {
@@ -14,8 +17,22 @@ public class gira_OBJ : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.left * OBJspeed * Time.deltaTime);
+        transform.Translate(Vector3.left * SaiAndando * Time.deltaTime);
 
-        transform.Rotate( 0,10 * Time.deltaTime * OBJspeedRot, 0);
+        transform.Rotate( 0,10 * Time.deltaTime * RotY, RotX);
+        
+
+
+        /*
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);*/
+
     }
 }
